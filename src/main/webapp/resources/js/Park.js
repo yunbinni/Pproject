2b96d73511eb61722053f42a05162e9a067ba8db
yunbinni <yunbin9049@gmail.com>
2021-06-21 11:15:10 +0900

    새 브랜치에 커밋

--- src/main/webapp/resources/js/Park.js
+++ src/main/webapp/resources/js/Park.js
@@ -4,5 +4,6 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
     center: new kakao.maps.LatLng(37.7578784, 127.0732659), //지도의 중심좌표.
     level: 4 //지도의 레벨(숫자가 커질수록 축소)
 };
-
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+//
