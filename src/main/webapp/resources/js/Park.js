54377d835e8d6aa12b8b20175dea0ba7c96a59be
yunbinni <yunbin9049@gmail.com>
2021-06-21 18:26:21 +0900

    지도에 전국주차장 표기

--- src/main/webapp/resources/js/Park.js
+++ src/main/webapp/resources/js/Park.js
@@ -2,7 +2,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        level: 10 // 지도의 확대 레벨(숫자가 작을수록 확대)
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
@@ -13,17 +13,19 @@ var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니
 var clusterer = new kakao.maps.MarkerClusterer({
     map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
     averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-    minLevel: 10 // 클러스터 할 최소 지도 레벨
+    minLevel: 6, // 클러스터 할 최소 지도 레벨
+    minClusterSize: 10 // 클러스터링할 최소 개수
 });
 
 // 데이터를 가져오기 위해 jQuery를 사용합니다
 // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
-$.get("/data/chicken.json", function(data) {
+$.get("/data/Park.json", function(data) {
     // 데이터에서 좌표 값을 가지고 마커를 표시합니다
     // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
     var markers = $(data.positions).map(function(i, position) {
         return new kakao.maps.Marker({
-            position : new kakao.maps.LatLng(position.lat, position.lng)
+            position : new kakao.maps.LatLng(position.위도, position.경도),
+            clickable : true
         });
     });
 
